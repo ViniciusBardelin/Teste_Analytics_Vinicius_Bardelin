21d11b190396b85abf489204dd4b9b8424202e68
ViniciusBardelin <125508443+ViniciusBardelin@users.noreply.github.com>
2024-11-23 15:51:45 -0300

    Update consultas_sql.sql

--- consultas_sql.sql
+++ consultas_sql.sql
@@ -6,20 +6,21 @@ USE quod
 #CREATE TABLE data_clean (
 #    ID INT,
 #    Data DATE,
-#    Produto CHAR(1),
+#    Produto CHAR(1), # Os produtos foram descritos com uma letra
 #    Categoria VARCHAR(15),
 #    Quantidade INT,
-#    Preco DECIMAL(10, 2)
+#    Preco DECIMAL(10, 2) # Alguns preços simulados possuem muitas casas decimais
 #);
 
+# Visualiza as 10 primeiras linhas da tabela "data_clean"
 SELECT * FROM data_clean LIMIT 10
 
-# Tarefa 1
+# Tarefa 1: Calcula o total de vendas por produto e categoria
 SELECT Produto, Categoria, SUM(Quantidade * Preco) AS Total_Vendas FROM data_clean
 GROUP BY Produto, Categoria
 ORDER BY Total_Vendas DESC;
 
-# Tarefa 2
+# Tarefa 2: Calcula o total de vendas no mês de junho
 SELECT Produto, Categoria, SUM(Quantidade * Preco) AS Total_Vendas_Junho FROM data_clean
 WHERE MONTH(Data) = 6 
 GROUP BY Produto, Categoria
